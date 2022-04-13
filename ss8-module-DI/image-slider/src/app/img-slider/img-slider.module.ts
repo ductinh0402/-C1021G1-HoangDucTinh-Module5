933bf36216487e4472f48bb9e39f3a09ea969353
ductinh0402 <hoangductinh0402@gmail.com>
2022-04-14 01:37:22 +0700

    push bai tap

---
+++ ss8-module-DI/image-slider/src/app/img-slider/img-slider.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImgSliderComponent } from './img-slider/img-slider.component';
+import { ImgSlideComponent } from './img-slider/img-slide/img-slide.component';
+
+
+
+@NgModule({
+  declarations: [ImgSliderComponent, ImgSlideComponent],
+  exports: [
+    ImgSlideComponent,
+    ImgSliderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ImgSliderModule { }
